6b91fd63cca9028b5d5950a0723386f55bd2338c
Caroline Taymor <ctaymor@gmail.com>
2014-02-01 21:01:05 -0800

    Updated SentenceReverserTest with feedback from code review Changed method name testReverseSentenceWithNoWordsJustPunctuation to testReverseSentenceMethodForRemovingPunctuation to better describe the function of the test, which is to test that reverseSentence removes all punctuation from the sentence correctly removes punctuation from a string which is all punctuation.
    Also instantiated SentenceReverser in SentenceReverserTest and corrected
    all tests so that reverserSentence is called on the instance of
    SentenceReverser.

--- ReverseSentence/edu/mills/mcs/reverse_sentence/SentenceReverserTest.java
+++ ReverseSentence/edu/mills/mcs/reverse_sentence/SentenceReverserTest.java
@@ -2,43 +2,45 @@ package edu.mills.mcs.reverse_sentence;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class SentenceReverserTest extends SentenceReverser {
+	SentenceReverser reverser = new SentenceReverser();
 	
 	@Test
 	public void testReverseSentenceWithEmptyString() {
 		String inputString ="";
-		assertEquals("" , reverseSentence(inputString));
+		assertEquals("" , reverser.reverseSentence(inputString));
 	}
 	
 	@Test
-	public void testReverseSentenceWithNoWordsJustPunctuation() {
+	public void testReverseSentenceMethodForRemovingPunctuation() {
 		String inputString =".,()";
-		assertEquals("", reverseSentence(inputString));
+		assertEquals("", reverser.reverseSentence(inputString));
 	}
 	
 	@Test
 	public void testReverseSentenceWithPunctuationAsItsOwnWord() {
 		String inputString ="The cat is a . fish.";
-		assertEquals("fish a is cat the", reverseSentence(inputString));
+		assertEquals("fish a is cat the", reverser.reverseSentence(inputString));
 	}
 	
 	@Test
 	public void testReverseSentenceWithSingleWordAndNoPunctuation() {
 		String inputString ="fish";
-		assertEquals("fish", reverseSentence(inputString));
+		assertEquals("fish", reverser.reverseSentence(inputString));
 	}
 
 	@Test
 	public void testReverseSentenceWithMultipleWordsAndNoPunctuation() {
 		String inputString ="One fish two fish";
-		assertEquals("fish two fish one", reverseSentence(inputString));
+		assertEquals("fish two fish one", reverser.reverseSentence(inputString));
 	}
 	
 	@Test
 	public void testReverseSentenceWithMultipleWordsAndPunctuation() {
 		String inputString ="One fish, two fish, red fish, blue fish.";
-		assertEquals("fish blue fish red fish two fish one", reverseSentence(inputString));
+		assertEquals("fish blue fish red fish two fish one", reverser.reverseSentence(inputString));
 	}
 }
